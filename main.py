8e3bc576b749d26a679bc1a9cd5c0db6fd7df2c5
Callum Eden <callum.eden15@imperial.ac.uk>
2019-04-10 10:36:14 +0100

    Timing the entire op

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from price_fetcher import PriceFetcher 
 import glob 
+import time
 
 data_history_start_date = '2010-07-17'
 
@@ -10,7 +11,12 @@ csv_path = input("Bitcoin block CSV file name or regex....")
 files = glob.glob(csv_path)
 print('******************* found files matching regex to be {} **************************'.format(files))
 
+start = time.time()
 for file in files:
 	print('============================== starting price fetching for file {} ================================='.format(file))
 	price_fetcher.write_csv_with_price_data(file)
 	print('============================== completed price fetching for file {} ================================'.format(file))
+	
+end = time.time()
+
+print('************** time elapsed = {} *********************'.format(end - start))
